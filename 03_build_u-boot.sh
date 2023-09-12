35a6d710815ec608d185aba513f09271124d5af3
Robert Nelson <robertcnelson@gmail.com>
2023-09-12 13:48:27 -0500

    cleanup: 03_build_u-boot.sh: better seperation between original and patched
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 03_build_u-boot.sh
+++ 03_build_u-boot.sh
@@ -7,28 +7,24 @@ CC=${CC:-"${wdir}/riscv-toolchain/bin/riscv64-linux-"}
 make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} distclean
 
 cd ./u-boot/
-#patch -p1 < ../patches/u-boot/0001-Use-MMUART0-for-stdout.patch
+#cp -v include/configs/microchip_mpfs_icicle.h ../patches/u-boot/original/
+#cp -v arch/riscv/dts/microchip-mpfs-icicle-kit.dts ../patches/u-boot/original/
+#cp -v configs/microchip_mpfs_icicle_defconfig ../patches/u-boot/original/
 #exit 2
-cp -v ../patches/u-boot/microchip_mpfs_icicle.h include/configs/microchip_mpfs_icicle.h
-cp -v ../patches/u-boot/microchip-mpfs-icicle-kit.dts arch/riscv/dts/
-cp -v ../patches/u-boot/microchip_mpfs_icicle_defconfig .config
+
+cp -v ../patches/u-boot/beaglev-fire/microchip_mpfs_icicle.h include/configs/microchip_mpfs_icicle.h
+cp -v ../patches/u-boot/beaglev-fire/microchip-mpfs-icicle-kit.dts arch/riscv/dts/
+cp -v ../patches/u-boot/beaglev-fire/microchip_mpfs_icicle_defconfig configs/microchip_mpfs_icicle_defconfig
 cd ../
 
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} microchip_mpfs_icicle
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} olddefconfig
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} menuconfig
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} savedefconfig
-#cp -v ./u-boot/defconfig ./u-boot/configs/microchip_mpfs_icicle_defconfig
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} distclean
-
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} microchip_mpfs_icicle_defconfig
+make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} microchip_mpfs_icicle_defconfig
 #make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} menuconfig
 
 make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} olddefconfig
 
 make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} savedefconfig
 cp -v ./u-boot/defconfig ./u-boot/configs/microchip_mpfs_icicle_defconfig
-cp -v ./u-boot/defconfig ./patches/u-boot/microchip_mpfs_icicle_defconfig
+cp -v ./u-boot/defconfig ./patches/u-boot/beaglev-fire/microchip_mpfs_icicle_defconfig
 
 echo "make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${CC} all"
 make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${CC} all
@@ -36,3 +32,4 @@ make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${CC} all
 cp -v ./u-boot/u-boot.bin ./deploy/
 cp -v ./u-boot/u-boot.bin ./deploy/src.bin
 
+#
