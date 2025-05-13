31575500110a2d6b2f760fc28c19a2f2c59ce325
Robert Nelson <robertcnelson@gmail.com>
2025-05-13 16:12:56 -0500

    rob, pull from everywhere
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull --rebase --no-edit https://github.com/beagleboard/BeagleV-Fire-ubuntu.git main
+git pull --rebase --no-edit https://openbeagle.org/beaglev-fire/BeagleV-Fire-ubuntu.git main
+git pull --rebase --no-edit https://gitlab.com/RobertCNelson/beaglev-fire-ubuntu.git main
