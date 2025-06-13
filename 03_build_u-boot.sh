95f440118495c52c6351591151502a077e58d122
Lars Randers <lranders@mail.dk>
2025-06-13 13:43:42 +0000

    u-boot: pull from u-boot-mchp
    - Point to official linux4microchip repo
    - Clean patches directory
    - Add back two patches from Jamie
    
    Signed-off-by: Lars Randers <lranders@mail.dk>

--- 03_build_u-boot.sh
+++ 03_build_u-boot.sh
@@ -4,19 +4,27 @@ CORES=$(getconf _NPROCESSORS_ONLN)
 wdir=`pwd`
 CC=${CC:-"${wdir}/riscv-toolchain/bin/riscv64-linux-"}
 
-make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} distclean
+cd ./u-boot/
 
-make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} microchip_mpfs_icicle_defconfig
-#make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} menuconfig
+if [ ! -f ./.patched ] ; then
+	if [ -f configs/beaglev_fire_defconfig ] ; then
+		git am ../patches/u-boot/0001-drivers-mailbox-mpfs-mbox-add-missing-include.patch
+		git am ../patches/u-boot/0002-board-beagle-beaglev_fire-fix-compilation-warning.patch
+	fi
+	touch .patched
+fi
 
-make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} olddefconfig
+make ARCH=riscv CROSS_COMPILE=${CC} distclean
 
-make -C u-boot ARCH=riscv CROSS_COMPILE=${CC} savedefconfig
-cp -v ./u-boot/defconfig ./u-boot/configs/microchip_mpfs_icicle_defconfig
-cp -v ./u-boot/defconfig ./patches/u-boot/beaglev-fire/microchip_mpfs_icicle_defconfig
+make ARCH=riscv CROSS_COMPILE=${CC} beaglev_fire_defconfig
+#make ARCH=riscv CROSS_COMPILE=${CC} menuconfig
 
-echo "make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${CC} all"
-make -C u-boot -j${CORES} ARCH=riscv CROSS_COMPILE=${CC} all
+make ARCH=riscv CROSS_COMPILE=${CC} olddefconfig
+
+echo "make -j${CORES} ARCH=riscv CROSS_COMPILE=${CC} all"
+make ARCH=riscv CROSS_COMPILE=${CC} all
+
+cd ../
 
 cp -v ./u-boot/u-boot.bin ./deploy/
 cp -v ./u-boot/u-boot.bin ./deploy/src.bin
